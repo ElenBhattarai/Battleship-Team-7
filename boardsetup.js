3a51d6b5290244358374e1652ca59bc16faa1599
Javante Ewing <j006e424@ku.edu>
2022-01-31 13:57:45 -0600

    Updated Program Structure

---
+++ boardsetup.js
@@ -0,0 +1,15 @@
+class boardSetup
+{
+    gridSetup()
+    {
+        //creates grids for playing
+    }
+    welcome()
+    {
+        //prompts user for how many ships they would like to play with
+    }
+    shipPlacement()
+    {
+        //loop for placing ships
+    }
+}
