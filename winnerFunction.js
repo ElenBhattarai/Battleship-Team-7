d61ba536afe731e8961b7c507e1456a5d7925155
Elen Bhattarai <83359964+ElenBhattarai@users.noreply.github.com>
2022-02-11 23:06:58 -0600

    Add files via upload

---
+++ winnerFunction.js
@@ -0,0 +1,19 @@
+let firstCount = 0;
+let secondCount = 0;
+function WinnerCountFirst()
+{
+    firstCount++;
+    return firstCount;
+}
+function WinnerCountSecond()
+{
+    secondCount++;
+    return secondCount;
+}
+function Winner(winner)
+{
+    if(winner == findTotalClickCount(maxClick))
+    {
+        //display prompt that 
+    }
+}
